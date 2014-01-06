10fa98920560453a2833886abae5139dee5563a9
Tanushree Jindal <tanushree.jindal@gmail.com>
2014-01-06 17:50:29 +0530

    Remove js subdirectory

--- js/mod_pubSub.js
+++
@@ -1,5 +0,0 @@
-// A basic pub-sub module. Any module which needs to publish/subscribe events should extend this module.
-// Currently, it is simply a reference to (and a trivially basic facade for) the Backbone's `Events` module.
-// (See vendor/backbone-events.js)
-
-_u.mod_pubSub = window.BackboneEvents;
